8ea75201bf1da273fbae86bf489ffda4b43de6d6
hlepps <pomapiotrek@gmail.com>
2024-04-04 11:19:15 +0200

    initial commit

---
+++ Source/project_prospector/project_prospectorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "project_prospectorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class Aproject_prospectorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	Aproject_prospectorGameMode();
+};
+
+
+
